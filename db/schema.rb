a9676a3d4c2037990e80102c7e3d0a8d493c0752
priyanka kamishetty <prynka543@gmail.com>
2021-07-29 07:30:47 +0530

    first comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_031303) do
+ActiveRecord::Schema.define(version: 2021_07_27_022330) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "pages"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
